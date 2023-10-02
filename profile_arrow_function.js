00e7070b6ba822ac5a0791a5149cf9672b3a062b
rian-haqi <141999480+rian-haqi@users.noreply.github.com>
2023-10-02 14:08:37 +0700

    Add files via upload

---
+++ profile_arrow_function.js
@@ -0,0 +1,7 @@
+const login = (namaDepan,namaBelakang,email) => {
+  console.log("Nama : " + namaDepan+" "+ namaBelakang)
+  console.log("email :"+ email)
+  console.log("Login Screen")
+}
+
+login("Rian","Haqi","222310275@smkn4-tsm.sch.id")
---
+++ profile_reg_function.js
@@ -0,0 +1,7 @@
+function home(namaDepan,namaBelakang,email) {
+  console.log("Nama : " + namaDepan +" "+ namaBelakang)
+  console.log("email :"+ email)
+  console.log("Selamat Datang di Home Screen")
+}
+
+home("Rian","Haqi","222310275@smkn4-tsm.sch.id")
